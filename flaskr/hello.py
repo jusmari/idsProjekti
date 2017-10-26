fd1cf0ba6a774e239bb4b241c1510f2d08817480
Nico Roos <nico.roos@gmail.com>
2017-10-26 14:51:44 +0300

    deploy

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    app.run()
